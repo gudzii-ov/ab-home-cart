c9de311773bfd47e12c4d20a3bc90415b5ceb79f
Oleg Gduzii <oleg198679@gmail.com>
2019-04-07 21:10:16 +0300

    Add total cost to state

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,4 @@ import { createAction } from 'redux-actions';
 export const addProduct = createAction('PRODUCT_ADD');
 export const editProduct = createAction('PRODUCT_EDIT');
 export const removeProduct = createAction('PRODUCT_REMOVE');
+export const updateTotalCost = createAction('TOTAL_COST_UPDATE');
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,26 +8,37 @@ import * as actions from '../actions';
 const products = handleActions({
   [actions.addProduct](state, { payload: { data: product } }) {
     const { byId, allIds } = state;
+    const newById = { ...byId, [product.id]: product };
+    const newAllIds = [...allIds, product.id];
+    const totalCost = _.sumBy(newAllIds, id => newById[id].price);
+
     return {
-      byId: { ...byId, [product.id]: product },
-      allIds: [...allIds, product.id],
+      byId: { ...newById },
+      allIds: [...newAllIds],
+      totalCost,
     };
   },
   [actions.editProduct](state, { payload: { data: product } }) {
     const { byId, allIds } = state;
+    const newById = { ...byId, [product.id]: product };
+
     return {
-      byId: { ...byId, [product.id]: product },
+      byId: { ...newById },
       allIds,
+      totalCost: _.sumBy(allIds, id => newById[id].price),
     };
   },
   [actions.removeProduct](state, { payload: { data: id } }) {
     const { byId, allIds } = state;
+    const newById = _.omitBy(byId, product => product.id === id);
+    const newAllIds = allIds.filter(currentId => currentId !== id);
     return {
-      byId: _.omitBy(byId, product => product.id === id),
-      allIds: allIds.filter(currentId => currentId !== id),
+      byId: { ...newById },
+      allIds: [...newAllIds],
+      totalCost: _.sumBy(newAllIds, subId => newById[subId].price),
     };
   },
-}, { byId: {}, allIds: [] });
+}, { byId: {}, allIds: [], totalCost: 0 });
 
 export default combineReducers({
   products,
