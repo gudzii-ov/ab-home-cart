0cfa00769c4d755671efabbdcf780c3eda5b270f
Oleg Gduzii <oleg198679@gmail.com>
2019-04-07 18:55:50 +0300

    Add app store

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import reducers from './reducers';
 import Cart from './components/Cart';
 
+/* eslint-disable no-underscore-dangle */
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__;
+const store = createStore(reducers, reduxDevtools && reduxDevtools());
+/* eslint-enable */
+
 const appElement = (
-  <Cart />
+  <Provider store={store}>
+    <Cart />
+  </Provider>
 );
 
 render(
   appElement,
-  document.querySelector('body'),
+  document.getElementById('cart'),
 );
